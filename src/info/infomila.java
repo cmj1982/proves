ba4da341b587c7df756b945331a967eae1b8640e
cmj1982 <carlesmj1982@gmail.com>
2016-02-24 11:23:34 +0100

    versio2.1

--- src/info/infomila.java
+++ src/info/infomila.java
@@ -18,6 +18,8 @@ public class infomila {
        System.out.println("1----------1");
        System.out.println("2----------2");
        System.out.println("3----------3");
+       System.out.println("2----------2");
+       System.out.println("3----------3");
     }
     
 }
